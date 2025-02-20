7a8911dc8a8938ea8a595f1da23614674065fe48
GR1SLy <nikonchyk@inbox.ru>
2025-02-20 19:15:56 +0700

    MakeFile for list

---
+++ lib/list/List.h
@@ -0,0 +1,16 @@
+#ifndef LIST_H
+#define LIST_H
+
+#pragma once
+
+class List
+{
+public:
+    List();
+    ~List();
+
+private:
+
+};
+
+#endif
---
+++ source.cpp
@@ -0,0 +1,8 @@
+#include "lib/list/List.h"
+#include "iostream"
+
+int main() {
+    List myList;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
